a961f69b60d14bb7b9c4545dfcd870baf9d0605e
Alana Alexander <laninsky@gmail.com>
2015-09-24 14:41:40 -0500

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,3 +1,5 @@
+ls *.1.fa | wc -l > numtaxa
+
 for i in `ls *1.fa`;
 do name=`echo $i | sed 's/.1.fa//'`;
 echo $name > tempnamefile;
@@ -12,4 +14,11 @@ do mv $i temp;
 mafft temp > $i;
 done;
 rm temp
+
+for i in *.fasta; 
+do name=`echo $i | sed 's/.fasta//'`;
+echo $name > tempnamefile;
+mv $i temp;
+Rscript step1B.R;
+rm -rf temp*;
 
