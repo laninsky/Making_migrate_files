0d84127c0f48064d9d95172ef3e83d9b1d25f48d
Alana Alexander <laninsky@gmail.com>
2015-09-24 13:53:56 -0500

    Create step1A.R

---
+++ step1A.R
@@ -0,0 +1,15 @@
+temp1 <- read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+temp2 <- read.table("temp2",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+samplename <- read.table("tempnamefile",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+
+rows <- dim(temp1)[1]
+sequencename1 <- paste(">",samplename[1,1],".1",sep="")
+sequencename2 <- paste(">",samplename[1,1],".2",sep="")
+
+for (i in 1:rows) {
+if ((length(grep(">",temp1[i,1])))>0) {
+outputname <- gsub(">","",temp1[i,1])
+output <- rbind(sequencename1, temp1[(i+1),1],sequencename2, temp2[(i+1),1])
+write.table(output, outputname,quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
+}
+}
