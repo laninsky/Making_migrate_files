1c1c10d382b27dc7b76df108a49c5ae13aebb32d
Alana Alexander <laninsky@gmail.com>
2015-09-24 13:57:12 -0500

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -3,6 +3,13 @@ do name=`echo $i | sed 's/.1.fa//'`;
 echo $name > tempnamefile;
 mv $i temp;
 mv $name.2.fa temp2;
-Rscript step1.R;
+Rscript step1A.R;
 rm -rf temp*;
 done
+
+for i in *.fasta; 
+do mv $i temp;
+mafft temp > $i;
+done;
+rm temp
+
