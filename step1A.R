4179f63cf8c0ad074507df63b4a6d9b0b200db98
Alana Alexander <laninsky@gmail.com>
2015-09-29 09:29:34 -0500

    Create step1A.R

---
+++ step1A.R
@@ -0,0 +1,34 @@
+temp1 <- read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+rows <- dim(temp1)[1]
+
+newname <- matrix(c("old_name","new_name"),ncol=2,nrow=1)
+sedcommands <- NULL
+numname <- 0
+
+for (i in 1:rows) {
+if ((length(grep(">",temp1[i,1])))>0) {
+if (nchar(temp1[i,1])>15) {
+lociname <- numname
+numname <- numname + 1
+} else {
+lociname <- gsub(">","",temp1[i,1])
+}
+if (!((length(grep("fasta",temp1[i,1])))>0)) {
+lociname <- paste(lociname,".fasta",sep="")
+}
+
+newnametemp <- cbind(temp1[i,1],lociname)
+newname <- rbind(newname,newnametemp)
+
+sedcommandstemp <- paste("sed -i 's/",temp1[i,1],"/",lociname,"/' $f;",sep="")
+sedcommands <- rbind(sedcommands,sedcommandstemp)
+}
+}
+
+script1 <- as.matrix(sedcommands)
+script1 <- rbind("for f in `ls *.fa`; do",script1,"done")
+
+write.table(sedcommands,"rename.sh",quote=FALSE, row.names=FALSE,col.names=FALSE)
+write.table(newname,"locikey",quote=FALSE, row.names=FALSE,col.names=FALSE)
+
+q()
