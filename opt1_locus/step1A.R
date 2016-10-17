02aa8d85707a2a2907f1cab27fb8d9604b4f657c
Alana Alexander <laninsky@gmail.com>
2016-10-17 17:09:45 -0500

    Update step1A.R

--- opt1_locus/step1A.R
+++ opt1_locus/step1A.R
@@ -1,34 +1,34 @@
-temp1 <- read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+temp1 <- as.matrix(read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t"))
 rows <- dim(temp1)[1]
 
-newname <- matrix(c("old_name","new_name"),ncol=2,nrow=1)
 sedcommands <- NULL
-numname <- 0
+numname <- 1
+
+temp1 <- cbind(temp1,"")
 
 for (i in 1:rows) {
-if ((length(grep(">",temp1[i,1])))>0) {
 if (nchar(temp1[i,1])>15) {
-lociname <- paste(">",numname,sep="")
+temp1[i,2] <- paste(">",numname,sep="")
 numname <- numname + 1
 } else {
-lociname <- temp1[i,1]
+temp1[i,2] <- temp1[i,1]
 }
-if (!((length(grep("fasta",temp1[i,1])))>0)) {
-lociname <- paste(lociname,".fasta",sep="")
 }
 
-newnametemp <- cbind(temp1[i,1],lociname)
-newname <- rbind(newname,newnametemp)
+rename <- temp1[which(temp1[,1]!=temp1[,2]),]
 
-sedcommandstemp <- paste("sed -i 's/",temp1[i,1],"/",lociname,"/' $f;",sep="")
+for (i in 1:dim(rename)[1]) {
+sedcommandstemp <- paste("sed -i 's/",rename[i,1],"/",rename[i,2],"/' $f;",sep="")
 sedcommands <- rbind(sedcommands,sedcommandstemp)
 }
-}
+
 
 script1 <- as.matrix(sedcommands)
 script1 <- rbind("for f in `ls *.fa`; do",script1,"done")
 
+temp1 <- rbind(c("old_name","new_name"),temp1)
+
 write.table(script1,"rename.sh",quote=FALSE, row.names=FALSE,col.names=FALSE)
-write.table(newname,"locikey",quote=FALSE, row.names=FALSE,col.names=FALSE)
+write.table(temp1,"locikey",quote=FALSE, row.names=FALSE,col.names=FALSE)
 
 q()
