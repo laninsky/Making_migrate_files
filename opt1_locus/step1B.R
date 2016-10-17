78a9c1b14b415be81ee370c13164ba6c64e37499
Alana Alexander <laninsky@gmail.com>
2016-10-17 18:57:10 -0500

    Update step1B.R

--- opt1_locus/step1B.R
+++ opt1_locus/step1B.R
@@ -8,7 +8,7 @@ sequencename2 <- paste(">",samplename[1,1],".2",sep="")
 
 for (i in 1:rows) {
 if ((length(grep(">",temp1[i,1])))>0) {
-outputname <- gsub(">","",temp1[i,1])
+outputname <- paste((gsub(">","",temp1[i,1])),".fasta",sep="")
 output <- rbind(sequencename1, temp1[(i+1),1],sequencename2, temp2[(i+1),1])
 write.table(output, outputname,quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
 }
